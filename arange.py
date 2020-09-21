25b0bcdf435e89ea574b88828792179878e6530b
Hani Ashour <33261274+4HaniAsh@users.noreply.github.com>
2020-09-21 12:16:52 +0300

    Add files via upload

---
+++ arange.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+myarray = np.arange(2,20,1,int )
+
+
+# * 2 => firstNo
+# * 20=>lastNO
+# * 1=>steps
+# * int=>Type
+
+# In[4]:
+
+
+print(myarray)
+
+
+# In[5]:
+
+
+myarray2 = np.arange(2,20,5,int )
+
+
+# In[6]:
+
+
+print(myarray2)
+
+
+# In[ ]:
+
+
+
+
